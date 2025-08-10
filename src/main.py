4e20677650d3641099da428c52ebce8750c9dcae
garrison <garrisoc@andrew.cmu.edu>
2025-08-10 14:18:17 -0700

    updated queries and states, implemented more of process/view

--- src/main.py
+++ src/main.py
@@ -17,47 +17,7 @@ with open(QUERIES_PATH, 'r') as file:
 with open(STATES_PATH, 'r') as file:
     STATES = json.load(file)
 
-# ---------------------------------- HELPER -----------------------------------
-class Args:
-    def __init__(self, args):
-        self.args = args[1:]
-        self.original = args[1:]
-
-    def decrement(self):
-        self.args = self.args[1:]
-
-    def check(self, num):
-        if len(self.args) < num:
-            raise IndexError('Expecting more arguments.')
-
-    def get(self):
-        return args.args[0]
-
-    def __len__(self):
-        return len(args.args)
-
-def parse_state_and_query(args):
-    state, query = None, None
-    if len(args) < 1:
-        return None, None
-
-    if args.get() not in STATES:
-        raise Exception("Argument is not a state.")
-
-    state = args.get()
-    args.decrement()
-
-    if len(args) == 1:
-        if args.get() not in QUERIES:
-            raise Exception("Query is invalid.")
-        query = args.get()
-        args.decrement()
-    elif len(args) > 1:
-        raise Exception("Too many arguments.")
-
-    args.decrement()
-    return state, query
-
+# -------------------------------- FILESYSTEM ---------------------------------
 def make_state_dir(state):
         time_str = time.strftime("%d-%m-%Y_%H:%M:%S")
         return os.path.join(RESULTS_PATH, f'{state}_{time_str}')
@@ -67,10 +27,43 @@ def get_paths(state_dir, query):
     json_path = os.path.join(dir_path, f'{query}.json')
     return dir_path, json_path
 
+def choose_timestamp(state):
+    directories = os.listdir(RESULTS_PATH)
+    directories = [ d for d in directories if state in d ]
+    if len(directories) == 0:
+        raise Exception("State does not exist.")
+    if len(directories) == 1:
+        return directories[0]
+
+    print("Choose a directory:")
+    for i in range(len(directories)):
+        subdirs = os.listdir(os.path.join(RESULTS_PATH, directories[i]))
+        print(f"\t{i} - {directories[i]} ({', '.join(subdirs)})")
+    index = int(input(">> "))
+    if not 0 <= index < len(directories):
+        raise Exception("Invalid choice.")
+    return directories[index]
 # -----------------------------------------------------------------------------
 
-def process():
-    pass
+def process(state, query, selection):
+    path = os.path.join(RESULTS_PATH, selection, query, f'{query}.json')
+    with open(path, 'r') as file:
+        query_results = json.load(file)
+ 
+def view(state, query, selection):
+    path = os.path.join(RESULTS_PATH, selection, query, f'{query}.json')
+    if not os.path.exists(path): return
+    with open(path, 'r') as file:
+        query_results = json.load(file)
+    
+    print(f'{selection}:')
+    print(f"\t{query_results['queries']['request'][0]['searchTerms']} ({query})\n")
+#    print(a['queries']['nextPage'])
+    for i in range(len(query_results['items'])):
+        website = query_results['items'][i]
+        print(f"{i+1} - {website['title']}")
+        print('\t' + website['link'])
+    print()
 
 def search_all():
     i = input('Are you sure? ')
@@ -108,46 +101,108 @@ def search_wrapper(state, query, state_dir = None):
         json.dump(result, f, indent=1)
 # -----------------------------------------------------------------------------
 
-if __name__ == "__main__":
-    args = Args(sys.argv)
 
-    args.check(1)
-    args.decrement
-    match args.get():
-        case 'process':
-            args.check(1)
-            args.decrement()
+# ----------------------------------- CLI -------------------------------------
+def next_except(args):
+    try:
+        return next(args)
+    except StopIteration:
+        return None
 
-            if args.get == 'all':
-                pass
+def parse_state_and_query(args):
+    state = next_except(args)
+
+    if state is None:
+        raise Exception("No state selected.")
+    elif state != 'all' and state not in STATES:
+        raise Exception("Argument is not a state.")
+
+    query = next_except(args)
+    if query and query not in QUERIES:
+        raise Exception("Query is invalid.")
+
+    if next_except(args) or (state == 'all' and query is not None):
+        raise Exception("Too many arguments.")
+
+    return state, query
+
+def main(args):
+    arg = next_except(args)
+    if arg is None:
+        raise Exception("Too few arguments.")
+
+    match arg:
+        case 'process' | 'view':
+            func = process if arg == 'process' else view
 
             state, query = parse_state_and_query(args)
-            process()
-        case 'search':
-            args.check(1)
-            args.decrement()
+            if state == 'all':
+                for path in os.listdir(RESULTS_PATH):
+                    state = path.split('_')[0]
+                    if state not in STATES: continue
+                    for query in QUERIES:
+                        func(state, query, path)
+                return
 
-            if args.get == 'all': search_all()
-
-            state, query = parse_state_and_query(args)
-            if state is None: 
-                raise Exception("No state selected.")
-
+            selection = choose_timestamp(state)
             if query:
+                func(state, query, selection)
+            else:
+                for query in QUERIES:
+                    func(state, query, selection)
+
+        case 'search':
+            state, query = parse_state_and_query(args)
+            if state == 'all': search_all()
+            elif query:
                 search_wrapper(state, query)
             else:
                 path = make_state_dir(state)
 
                 for query in QUERIES:
-                    print(type(query))
                     search_wrapper(state, query, path)
+
+        case 'list':
+            state, query = parse_state_and_query(args)
+            if state == 'all': 
+                print('Results:')
+                for p in os.listdir(RESULTS_PATH):
+                    queries = os.listdir(os.path.join(RESULTS_PATH, p))
+                    print(f'\t{p} ({", ".join(queries)})')
+            else:
+                states_dirs = [os.path.join(RESULTS_PATH, d)
+                               for d in os.listdir(RESULTS_PATH) 
+                               if state in d]
+                for state_dir in states_dirs:
+                    queries = os.listdir(state_dir)
+
+                    if not query:
+                        print(f'Queries: ({state_dir})')
+                        for q in queries:
+                            print('\t' + q)
+                    else:
+                        if query not in queries:
+                            raise Exception("Query is invalid.")
+                        
+                        files = os.listdir(os.path.join(RESULTS_PATH, state_dir,
+                                                        query))
+                        print(f'Files: ({state_dir}/{query})')
+                        for f in files:
+                            print('\t' + f)
+
+        case 'help':
+            print("process {state/all} [query]\n"
+                  "view {state/all} [query]\n"
+                  "search {state/all} [query]")
         case _:
             print("Invalid input.")
 
     if LOGGING:
         with open(LOGGING, 'a') as file:
             time_str = time.strftime("%d-%m-%Y_%H:%M:%S")
-            text = time_str + '\t' + " ".join(sys.argv)
+            text = '\n' + time_str + '\t' + " ".join(sys.argv)
             file.write(text)
 
-
+if __name__ == "__main__":
+    main(iter(sys.argv[1:]))
+# -----------------------------------------------------------------------------
