c925bf6be8dbdfb522350d4bbc98065a432e9a80
garrison <garrisoc@andrew.cmu.edu>
2026-06-22 07:11:45 -0700

    cleaned results text; developed some individual getters

--- verify/src/combine.py
+++ verify/src/combine.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 import os, time
 import json, csv
+import re
 
 from util import get_most_recent_rdirs
+from constants import *
 
-SEARCH_RESULTS_PATH = os.getenv('SEARCH_RESULTS_PATH')
-SEARCH_RESULTS_PARSED = os.getenv('SEARCH_RESULTS_PARSED')
+import pprint
 
-AIRTABLE_RESULTS = os.getenv('AIRTABLE_RESULTS')
-AIRTABLE_TO_JSON = os.getenv('AIRTABLE_TO_JSON')
+def split_string(response) -> list():
+    split = response.splitlines()
+    parsed = {}
+    link_re = re.compile(r".*(http.*)")
 
-RESULTS = os.getenv('RESULTS')
-QUERIES_TO_QUESTIONS = os.getenv('QUERIES_TO_QUESTIONS')
+    link = None
+    val = ''
+    for i in range(len(split)):
+        match = link_re.match(split[i])
+        if match:
+            if link:
+                parsed[link] = val if val != '' else None
+                val = ''
+            link = match[1]
+        else:
+            val = "\n".join(filter(None, [val, split[i]]))
+    if val:
+        parsed[link] = val if val != '' else None
+
+    print(parsed)
 
 def get_queries_from_search() -> dict():
     states = dict()
@@ -80,10 +96,13 @@ def get_airtable() -> dict():
         header = next(reader)
 
         for row in reader:
-            state = row[0]
+            state = row[0].strip()
+            if state == 'DC': state = "District of Columbia"
+            if state in states:
+                state = state + "_1"
             states[state] = {}
-            for i in range(len(row[1:])):
-                states[state][airtable_map[str(i)]] = row[i]
+            for i in range(len(row)):
+                states[state][airtable_map[str(i)]] = row[i].strip()
 
 #            print(states[state])
 
@@ -91,13 +110,77 @@ def get_airtable() -> dict():
 
 def dump_search_results():
     results = get_parsed(get_queries_from_search())
-    with open(RESULTS, 'w') as file:
+    with open(SEARCH_RESULTS_JSON, 'w') as file:
         json.dump(results, file, indent=1)
 
 def dump_airtable_results():
     results = get_airtable()
-    with open(RESULTS, 'w') as file:
+    with open(AIRTABLE_RESULTS_JSON, 'w') as file:
         json.dump(results, file, indent=1)
 
+def map_to_questions():
+    with open(AIRTABLE_TO_QUESTIONS, 'r') as file:
+        airtable_map = json.load(file)
+    with open(QUERIES_TO_QUESTIONS, 'r') as file:
+        queries_map = json.load(file)
+    with open(SEARCH_RESULTS_JSON, 'r') as file:
+        search_results_json = json.load(file)
+    with open(AIRTABLE_RESULTS_JSON, 'r') as file:
+        airtable_results_json = json.load(file)
 
-dump_airtable_results()
+    states = dict()
+
+    # accounts for missing states in either list & duplicates in Airtable
+    states_list = search_results_json.keys() | airtable_results_json.keys()
+
+    for state in states_list:
+        states[state] = dict()
+        state_dict = airtable_results_json.get(state)
+        if state_dict is None: continue
+        for category in airtable_map:
+            states[state][category] = dict()
+            for subcategory in airtable_map[category]:
+                states[state][category][subcategory] = dict()
+                state_result_dict = states[state][category][subcategory] 
+                
+                if state_dict[subcategory] != '':
+                    if state_dict[subcategory] == 'checked':
+                        state_result_dict["value"] = True
+                    else:
+                        state_result_dict["value"] = state_dict[subcategory]
+
+                if airtable_map[category][subcategory] is None: continue
+
+                if ("Source" in airtable_map[category][subcategory] and 
+                    state_dict[airtable_map[category][subcategory]["Source"]] != ""):
+                    state_result_dict["AirtableSource"] = state_dict[airtable_map[category][subcategory]["Source"]]
+                    print(category, subcategory)
+                    split_string(state_result_dict["AirtableSource"])
+
+                if ("Other" in airtable_map[category][subcategory]
+                    and state_dict[airtable_map[category][subcategory]["Other"]] != ""):
+                    state_result_dict["AirtableOther"] = state_dict[airtable_map[category][subcategory]["Other"]]
+#                    if state_result_dict["AirtableOther"] == "":
+#                        del state_result_dict["Airtable_source"] 
+
+
+#                for val in airtable_map[category][subcategory]:
+#                    #                    if state_dict[airtable_map[category][subcategory][val]]
+#
+#                    state_result_dict[val] = state_dict[airtable_map[category][subcategory][val]]
+
+#        for val in states[state]:
+#            print(f'{val} - {states[state][val]}')
+#        print(states[state])
+#        pprint.pp(states[state], width=180)
+
+
+#    print(queries_map)
+
+
+
+#    return airtable_map
+
+
+# dump_airtable_results()
+# map_to_questions()
